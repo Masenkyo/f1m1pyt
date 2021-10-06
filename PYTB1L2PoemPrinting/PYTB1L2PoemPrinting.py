937995a5519268592ec1c9483990112c1313e9ad
Masenkyo <33390@ma-web.nl>
2021-10-06 12:54:26 +0200

    second commit

---
+++ PYTB1L2PoemPrinting/PYTB1L2PoemPrinting.py
@@ -0,0 +1,8 @@
+vers1 = "Ik ben Sil Wever, "
+vers2 = "Ik woon in Den Helder, "
+vers3 = "Ik hou van gamen, "
+vers4 = "En heb een hond, "
+vers5 = "Die heet Billy."
+
+print(vers1,vers2, vers3)
+print(vers4,vers5)
