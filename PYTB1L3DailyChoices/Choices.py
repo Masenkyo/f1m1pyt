c35215f6da43dd4a053ffddd924e265a72049238
Masenkyo <33390@ma-web.nl>
2021-10-08 10:09:43 +0200

    sixth commit

---
+++ PYTB1L3DailyChoices/Choices.py
@@ -0,0 +1,8 @@
+print("You come home, do you TURN ON ur pc or go HAVE REST ?")
+choice = input()
+if choice == 'TURN ON':
+    print("U had a good game night.")
+elif choice == 'HAVE REST':
+    print("U had a good nights rest")
+else:
+    print(choice, " wasn't a valid choice")
